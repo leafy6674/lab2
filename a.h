d769be2f407908194e8f432d110f24e712e29a9e
Student <student@lab-315-018.cs.csubak.edu>
2018-02-02 15:19:19 -0800

    my crazy asteroid

---
+++ a.h
@@ -0,0 +1,309 @@
+
+
+//
+//
+//
+//
+//program: asteroids.cpp
+//author:  Gordon Griesel
+//date:    2014 - 2018
+//mod spring 2015: added constructors
+//mod spring 2018: X11 wrapper class
+//This program is a game starting point for a 3350 project.
+//
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cmath>
+using namespace std;
+#include <unistd.h>
+#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "log.h"
+#include "fonts.h"
+
+//defined types
+typedef float Flt;
+typedef float Vec[3];
+typedef Flt	Matrix[4][4];
+
+//macros
+#define rnd() (((Flt)rand())/(Flt)RAND_MAX)
+#define random(a) (rand()%(a))
+#define VecZero(v) (v)[0]=0.0,(v)[1]=0.0,(v)[2]=0.0
+#define MakeVector(x, y, z, v) (v)[0]=(x),(v)[1]=(y),(v)[2]=(z)
+#define VecCopy(a,b) (b)[0]=(a)[0];(b)[1]=(a)[1];(b)[2]=(a)[2]
+#define VecDot(a,b)	((a)[0]*(b)[0]+(a)[1]*(b)[1]+(a)[2]*(b)[2])
+#define VecSub(a,b,c) (c)[0]=(a)[0]-(b)[0]; \
+						(c)[1]=(a)[1]-(b)[1]; \
+						(c)[2]=(a)[2]-(b)[2]
+//constants
+const float TIMESLICE = 1.0f;
+const float GRAVITY = -0.2f;
+#define PI 3.141592653589793
+#define ALPHA 1
+const int MAX_BULLETS = 11;
+const Flt MINIMUM_ASTEROID_SIZE = 60.0;
+
+//-----------------------------------------------------------------------------
+//Setup timers
+const double OOBILLION = 1.0 / 1e9;
+extern struct timespec timeStart, timeCurrent;
+extern double timeDiff(struct timespec *start, struct timespec *end);
+extern void timeCopy(struct timespec *dest, struct timespec *source);
+//-----------------------------------------------------------------------------
+
+class Global {
+public:
+	int xres, yres;
+	char keys[65536];
+	Global() {
+		xres = 1250;
+		yres = 900;
+		memset(keys, 0, 65536);
+	}
+
+    
+} gl;
+
+class Ship {
+public:
+	Vec dir;
+	Vec pos;
+	Vec vel;
+	float angle;
+	float color[3];
+public:
+	Ship() {
+		VecZero(dir);
+		pos[0] = (Flt)(gl.xres/2);
+		pos[1] = (Flt)(gl.yres/2);
+		pos[2] = 0.0f;
+		VecZero(vel);
+		angle = 0.0;
+		color[0] = color[1] = color[2] = 1.0;
+	}
+};
+
+class Bullet {
+public:
+	Vec pos;
+	Vec vel;
+	float color[3];
+	struct timespec time;
+public:
+	Bullet() { }
+};
+
+class Asteroid {
+public:
+	Vec pos;
+	Vec vel;
+	int nverts;
+	Flt radius;
+	Vec vert[8];
+	float angle;
+	float rotate;
+	float color[3];
+	struct Asteroid *prev;
+	struct Asteroid *next;
+public:
+	Asteroid() {
+
+
+		prev = NULL;
+		next = NULL;
+	}
+};
+
+class Game {
+public:
+	Ship ship;
+	Asteroid *ahead;
+	Bullet *barr;
+	int nasteroids;
+	int nbullets;
+	struct timespec bulletTimer;
+	struct timespec mouseThrustTimer;
+	bool mouseThrustOn;
+    int destroyedAsteroid  ; // JT Step 3 
+public:
+	Game() {
+        destroyedAsteroid = 0 ; // JT Step 3 
+		ahead = NULL;
+		barr = new Bullet[MAX_BULLETS];
+		nasteroids = 0;
+		nbullets = 0;
+		mouseThrustOn = false;
+		//build 10 asteroids...
+		for (int j=0; j<10; j++) {
+			Asteroid *a = new Asteroid;
+            
+     
+
+			a->nverts = 8;
+			a->radius = rnd()*80.0 + 40.0;
+			Flt r2 = a->radius / 2.0;
+			Flt angle = 0.0f;
+			Flt inc = (PI * 2.0) / (Flt)a->nverts;
+
+            a->color[0] = rnd();  // JT: Step 1 Change Asteroid Color
+	        a->color[1] = rnd();
+	        a->color[2] = rnd();
+
+             
+
+			for (int i=0; i<a->nverts; i++) {
+				a->vert[i][0] = sin(angle) * (r2 + rnd() * a->radius);
+				a->vert[i][1] = cos(angle) * (r2 + rnd() * a->radius);
+				angle += inc;
+			}
+
+        
+
+			a->pos[0] = (Flt)(rand() % gl.xres);
+			a->pos[1] = (Flt)(rand() % gl.yres);
+			a->pos[2] = 0.0f;
+			a->angle = 0.0;
+			a->rotate = rnd() * 4.0 - 2.0;
+	
+
+
+			a->vel[0] = (Flt)(rnd()*2.0-1.0);
+			a->vel[1] = (Flt)(rnd()*2.0-1.0);
+			//std::cout << "asteroid" << std::endl;
+			//add to front of linked list
+			a->next = ahead;
+			if (ahead != NULL)
+				ahead->prev = a;
+			ahead = a;
+			++nasteroids;
+		}
+		clock_gettime(CLOCK_REALTIME, &bulletTimer);
+	}
+	~Game() {
+		delete [] barr;
+	}
+} g;
+
+//X Windows variables
+class X11_wrapper {
+private:
+	Display *dpy;
+	Window win;
+	GLXContext glc;
+public:
+	X11_wrapper() {
+		GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
+		//GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, None };
+		XSetWindowAttributes swa;
+		setup_screen_res(gl.xres, gl.yres);
+		dpy = XOpenDisplay(NULL);
+		if (dpy == NULL) {
+			std::cout << "\n\tcannot connect to X server" << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		Window root = DefaultRootWindow(dpy);
+		XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
+		if (vi == NULL) {
+			std::cout << "\n\tno appropriate visual found\n" << std::endl;
+			exit(EXIT_FAILURE);
+		} 
+		Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
+		swa.colormap = cmap;
+		swa.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask |
+			PointerMotionMask | MotionNotify | ButtonPress | ButtonRelease |
+			StructureNotifyMask | SubstructureNotifyMask;
+		win = XCreateWindow(dpy, root, 0, 0, gl.xres, gl.yres, 0,
+				vi->depth, InputOutput, vi->visual,
+				CWColormap | CWEventMask, &swa);
+		set_title();
+		glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
+		glXMakeCurrent(dpy, win, glc);
+		show_mouse_cursor(0);
+	}
+	~X11_wrapper() {
+		XDestroyWindow(dpy, win);
+		XCloseDisplay(dpy);
+	}
+	void set_title() {
+		//Set the window title bar.
+		XMapWindow(dpy, win);
+		XStoreName(dpy, win, "Asteroids template");
+	}
+	void check_resize(XEvent *e) {
+		//The ConfigureNotify is sent by the
+		//server if the window is resized.
+		if (e->type != ConfigureNotify)
+			return;
+		XConfigureEvent xce = e->xconfigure;
+		if (xce.width != gl.xres || xce.height != gl.yres) {
+			//Window size did change.
+			reshape_window(xce.width, xce.height);
+		}
+	}
+	void reshape_window(int width, int height) {
+		//window has been resized.
+		setup_screen_res(width, height);
+		glViewport(0, 0, (GLint)width, (GLint)height);
+		glMatrixMode(GL_PROJECTION); glLoadIdentity();
+		glMatrixMode(GL_MODELVIEW); glLoadIdentity();
+		glOrtho(0, gl.xres, 0, gl.yres, -1, 1);
+		set_title();
+	}
+	void setup_screen_res(const int w, const int h) {
+		gl.xres = w;
+		gl.yres = h;
+	}
+	void swapBuffers() {
+		glXSwapBuffers(dpy, win);
+	}
+	bool getXPending() {
+		return XPending(dpy);
+	}
+	XEvent getXNextEvent() {
+		XEvent e;
+		XNextEvent(dpy, &e);
+		return e;
+	}
+	void set_mouse_position(int x, int y) {
+		XWarpPointer(dpy, None, win, 0, 0, 0, 0, x, y);
+	}
+	void show_mouse_cursor(const int onoff) {
+		if (onoff) {
+			//this removes our own blank cursor.
+			XUndefineCursor(dpy, win);
+			return;
+		}
+		//vars to make blank cursor
+		Pixmap blank;
+		XColor dummy;
+		char data[1] = {0};
+		Cursor cursor;
+		//make a blank cursor
+		blank = XCreateBitmapFromData (dpy, win, data, 1, 1);
+		if (blank == None)
+			std::cout << "error: out of memory." << std::endl;
+		cursor = XCreatePixmapCursor(dpy, blank, blank, &dummy, &dummy, 0, 0);
+		XFreePixmap(dpy, blank);
+		//this makes you the cursor. then set it using this function
+		XDefineCursor(dpy, win, cursor);
+		//after you do not need the cursor anymore use this function.
+		//it will undo the last change done by XDefineCursor
+		//(thus do only use ONCE XDefineCursor and then XUndefineCursor):
+	}
+} x11;
+
+//function prototypes
+void init_opengl();
+void check_mouse(XEvent *e);
+int check_keys(XEvent *e);
+void physics();
+void render();
+
+
